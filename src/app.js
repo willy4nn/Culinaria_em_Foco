9bbe7e2bd4bbf7782c8c0ea37baeaee874ba0e81
Willyan <willy4nnp@gmail.com>
2024-04-17 21:44:27 -0300

    add route home page

--- src/app.js
+++ src/app.js
@@ -36,6 +36,10 @@ app.get('/login', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
 app.get('/post', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
