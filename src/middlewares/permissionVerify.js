d2a826a1abe20943c745e4c909ebdc633addf776
Leandro Santos <gaiden_leo@hotmail.com>
2024-05-08 17:48:29 -0300

    small update on permission verify

--- src/middlewares/permissionVerify.js
+++ src/middlewares/permissionVerify.js
@@ -6,12 +6,12 @@ const SECRET_KEY = config.SECRET_KEY;
 async function permissionVerify (req, res, next){
     const sessionToken = req.cookies.session_id;
     if(!sessionToken){
-        return res.status(401).redirect("/");
+        return res.status(401).json({ error: "Token JWT ausente" });
     }
 
     await jwt.verify(sessionToken, SECRET_KEY, (err, decoded) => {
         if(err){
-            return res.status(403).redirect("/");
+            return res.status(401).json({ error: "Token JWT inválido" });
         }else{
             req.user = decoded.user;
             next();
