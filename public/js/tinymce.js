40bb006d5e21d4473b32c41712291c893593cd94
Leandro Santos <gaiden_leo@hotmail.com>
2024-05-10 18:18:21 -0300

    updated tinyMCE

--- public/js/tinymce.js
+++ public/js/tinymce.js
@@ -19,7 +19,7 @@ tinymce.init({
     selector: '#mytextarea',
     language: 'pt_BR',
     plugins: 'preview importcss tinydrive searchreplace autolink autosave save directionality visualblocks visualchars fullscreen image link media codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars linkchecker emoticons',
-    tinydrive_token_provider: '/tinymce-jwt',
+    tinydrive_token_provider: '/api/uploads/tinymce-jwt',
     toolbar: "undo redo | revisionhistory | aidialog aishortcuts | blocks fontsizeinput | bold italic | align numlist bullist | link image | table media | lineheight  outdent indent | strikethrough forecolor backcolor removeformat | charmap emoticons | code fullscreen preview | save print | pagebreak anchor codesample | addtemplate inserttemplate | addcomment showcomments | ltr rtl casechange | spellcheckdialog",
     mobile: {
         plugins: 'preview importcss tinydrive searchreplace autolink autosave save directionality visualblocks visualchars fullscreen image link media codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars linkchecker emoticons',
