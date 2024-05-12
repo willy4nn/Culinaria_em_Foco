22d93caa985137388e1b501b35c9f351530108e8
Leandro Santos <gaiden_leo@hotmail.com>
2024-05-12 10:30:19 -0300

    refactoring image uploads system

--- src/utils/filesUpload.js
+++ src/utils/filesUpload.js
@@ -4,67 +4,84 @@ const path = require('path');
 const fs = require('fs');
 
 // Define o caminho absoluto para a pasta "public"
-const publicPath = path.join(__dirname, '../public');
+const publicPath = path.join(__dirname, '../../public');
+
+// Função utilitária para criar diretórios recursivamente se não existirem
+function ensureDirectoryExists(directory) {
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory, { recursive: true });
+  }
+}
 
 //Upload de imagens com o Multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     try {
       let uploadPath;
-      if (req.body.type === 'banner') 
-        uploadPath = './public/uploads/posts_banner';
-      else if (req.body.type === 'photo')
-        uploadPath = './public/uploads/profile_photo';
-      else 
-        throw Error('Tipo de upload inválido (banner | photo)');
-      
-      // Verificar se o diretório existe, caso contrário, criá-lo
-      if (!fs.existsSync(uploadPath)) {
-        fs.mkdirSync(uploadPath, { recursive: true });
+      switch (req.body.type) {
+        case 'banner':
+          uploadPath = path.join(publicPath, 'uploads/posts_banner');
+          break;
+        case 'photo':
+          uploadPath = path.join(publicPath, 'uploads/profile_photo');
+          break;
+        default:
+          throw new Error('Tipo de upload inválido (banner | photo)');
       }
 
-      cb(null, uploadPath);
+      ensureDirectoryExists(uploadPath);
 
+      cb(null, uploadPath);
     } catch (error) {
-      console.error('Erro :', error)
+      console.error('Erro :', error);
+      cb(error);
     }
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix)
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
   }
 });
 
 const multerUpload = multer({ storage });
 
 function multerUploadFiles(req, res) {
-    res.json({ message: "Successfully uploaded files", data: { 
-      'destination': req.file.destination,
-      'filename': req.file.filename 
-    }}
-  )};
+  res.json({ message: "Successfully uploaded files", data: { 
+    'destination': req.file.destination,
+    'filename': req.file.filename 
+  }});
+}
 
 //Upload de imagens com o axios
 async function axiosUploadFiles(req, res) {
-    const { imageUrls, imageUris } = req.body;
-  
-    // Iterar sobre as URLs das imagens e fazer o download e salvar localmente
-    for (let i = 0; i < imageUrls.length; i++) {
-      try {
-        const response = await axios.get(imageUrls[i], { responseType: 'arraybuffer' });
-        const directory = './public/uploads/posts_media';
-  
-        // Verificar se o diretório existe, caso contrário, criá-lo
-        if (!fs.existsSync(directory)) {
-          fs.mkdirSync(directory, { recursive: true });
+  const { imageUrls, imageUris } = req.body;
+  const downloadPromises = [];
+
+  // Iterar sobre as URLs das imagens e fazer o download e salvar localmente
+  for (let i = 0; i < imageUrls.length; i++) {
+    downloadPromises.push(
+      new Promise(async (resolve, reject) => {
+        try {
+          const response = await axios.get(imageUrls[i], { responseType: 'arraybuffer' });
+          const directory = path.join(publicPath, 'uploads/posts_media');
+          ensureDirectoryExists(directory);
+          const filename = path.join(directory, imageUris[i]);
+          fs.writeFileSync(filename, Buffer.from(response.data, 'binary'));
+          resolve();
+        } catch (error) {
+          console.error("Imagem não foi enviada!", error);
+          reject(error);
         }
-  
-        const filename = path.join(directory, imageUris[i]);
-        fs.writeFileSync(filename, Buffer.from(response.data, 'binary'));
-      } catch (error) {
-        console.error("Imagem não foi enviada!", error)
-      res.sendFile(path.join(publicPath, 'index.html'));
-    }}
+      })
+    );
   }
+
+  try {
+    await Promise.all(downloadPromises);
+    res.json({ message: "Imagens enviadas com Sucesso!" });
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao enviar as Imagens" });
+  }
+}
 
 module.exports = { multerUploadFiles, multerUpload, axiosUploadFiles };
